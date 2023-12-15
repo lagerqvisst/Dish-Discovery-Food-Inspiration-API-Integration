fe2753ee4d7ed54998105f1ec372e1a1f351c72f
lagerqvisst <108764890+lagerqvisst@users.noreply.github.com>
2023-12-15 12:36:05 +0100

    commit on main

--- javascript/apiextract.js
+++ javascript/apiextract.js
@@ -1,18 +1,55 @@
-//Som beskrivet i toppen av "index.js" har vi valt att lägga implementationen av api-anropet i sen separat js-fil.
+export function MealByIngridient(string) {
 
-//Följande funktion utgör en sk. "promise-kedja" där vi asynkront väntar på svar i olika led innan vi går vidare till nästa tills vi kommer i mål.
-// Steg 1: Fetch -> en asynkron HTTP-förfrågan till länken nedan som vi väntar på att slutföras innan vi går vidare till steg 2.
-// Steg 2: Om HTTP-förfrågan lyckas, startar vi ett nytt block i promise-kedjan där vi säger "konvertera" HTTP-svaret till en JSON-fil.
-// Steg 3: När vi väntat klart på konverteringen till JSON så vill vi slutligen retunera JSON-datan till den som anropat metoden.
-// Steg (4): Notera i anropet i "index.js" att vi också använder "then" vilket betyder att vi väntar på att alla steg i denna kedjan ska slutföras
-//           innan vi börjar bearbeta datan.
+    return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${string}`)
+      
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      return data;
+    })
+  
+  };
 
-export function getStrykTipsData(){
-    return fetch(' https://stryk.herokuapp.com/strycket2023') //Steg 1
-        .then(function (response){                            //Steg 2
-            return response.json();
-        })
-        .then(function (data){                                //Steg 3
-            return data;
-        })
-}
+  export function MealByID(string) {
+
+    return fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${string}`)
+      
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      return data;
+    })
+  
+  };
+
+
+
+
+  export function MealByountryt(string) {
+
+    return fetch(`www.themealdb.com/api/json/v1/1/filter.php?a=${string}`)
+      
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      return data;
+    })
+  
+  };
+
+  
+  export function RandomMeal() {
+  
+    return fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      return data;
+    })
+  
+  };
--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,75 @@
+import { MealByIngridient } from './apiextract.js';
+import { MealByID } from './apiextract.js';
+import { RandomMeal } from './apiextract.js';
+//Funktionalitet för Discover Dishes sidan där man kan söka på en ingridens för att returnerar resultat.
 
-import {  } from '/apiextract.js';
+const searchInput = document.getElementById('searchFieldIngridient');
+const searchButton = document.getElementById('ingridientButtonSubmit');
 
+const randomMealButton = document.getElementById('randomMealButton');
 
-//För att lyckas med denna uppgift behöver vi programmera asynkront eftersom att just datahämtning från ett API är en asynkron uppgift. 
-//Pusselbiten här är ".then" som säkerställer att vi inte börjar skapa några matchrader förrens datahämtningen är klar (promise/löftet har levererats).
-//Beskriver i mer detalj på "fetch.Data.js" om hur metoden är utformad.
-getStrykTipsData().then(function (data) {
-    //När datahämtningen är fullständig kör vi metoden nedan för att skapa matchraderna (callback-funktion).  
-    SkapaMatchRader(data);
+//Sektion som agerar parent där vi lägger till sökresultaten. 
+const searchResultsSection = document.getElementById("searchResults"); 
+
+//Tar in användarens input som sträng och skickar in den i metoden för att hämta specifik data från API baserat på text strängen.
+searchButton.addEventListener('click', function() {
+    // Spara ner användarens input i sökfältet som skickas vidare till API-metoden. 
+    var searchValue = searchInput.value;
+    console.log('Söksträng: ', searchValue);
+
+    MealByIngridient(searchValue).then(function (data) {
+        ClearResults(); 
+        GenerateMealData(data); 
+    });
 });
+
+randomMealButton.addEventListener('click', function() {
+    
+    RandomMeal().then(function (data){
+        ClearResults(); 
+        GenerateMealData(data); 
+    })
+    
+});
+
+//Denna metod är skapad för att rensa innehållet när man tex. trycker på random-knappen flera gånger. 
+//Även om du tryckt på random och sedan börjar söka så vill jag ta bort resultatet från random knappen.
+function ClearResults() {
+    const searchResultsSection = document.getElementById('searchResults');
+    searchResultsSection.innerHTML = ''; 
+}
+
+
+async function GenerateMealData(data) {
+
+    for (const meal of data.meals) {
+        var idMeal = meal.idMeal;
+
+        const mealDetails = await MealByID(idMeal);
+        console.dir(mealDetails);
+
+        const articleContainer = document.createElement('article');
+        articleContainer.className = 'recipeArticle';
+
+        const mealName = document.createElement('h2');
+        mealName.className = 'foodTitleSearch'; 
+        mealName.textContent = meal.strMeal;
+
+        const mealImage = document.createElement('img');
+        mealImage.className = 'foodImagesearch'; 
+        mealImage.src = meal.strMealThumb;
+
+        const instructions = document.createElement('p');
+        instructions.className = 'reciepeSearch'; 
+        instructions.textContent = mealDetails.meals[0].strInstructions;
+
+        articleContainer.appendChild(mealName);
+        articleContainer.appendChild(mealImage);
+        articleContainer.appendChild(instructions);
+
+        searchResultsSection.appendChild(articleContainer);
+    }
+}
+
+
+
